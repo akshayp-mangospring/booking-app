ef72a2e92fd471c7fa749ebe38a6ff6e5d534e4d
akshayp-mangospring <akshayp@mangospring.com>
2022-07-15 23:08:55 +0530

    Add basic function to book tickets and reduce remaining tickets count

--- main.go
+++ main.go
@@ -12,6 +12,19 @@ func main() {
 	fmt.Println("Welcome to our conference booking application!")
 	fmt.Println(getMarketingLine(confName))
 	fmt.Printf("We have %d tickets left. Book quickly to get your spot!\n", remainingTickets)
+
+	bookTicket("Akshay", 5, &remainingTickets)
+	bookTicket("Sourabh", 5, &remainingTickets)
+	bookTicket("Nivedita", 5, &remainingTickets)
+	bookTicket("Akshay", 5, &remainingTickets)
+	bookTicket("Sourabh", 5, &remainingTickets)
+	bookTicket("Nivedita", 5, &remainingTickets)
+	bookTicket("Akshay", 5, &remainingTickets)
+	bookTicket("Sourabh", 5, &remainingTickets)
+	bookTicket("Nivedita", 5, &remainingTickets)
+	bookTicket("Akshay", 5, &remainingTickets)
+	bookTicket("Sourabh", 5, &remainingTickets)
+	bookTicket("Nivedita", 5, &remainingTickets)
 }
 
 func getMarketingLine(s string) string {
@@ -22,4 +35,14 @@ func getMarketingLine(s string) string {
 	*/
 
 	return fmt.Sprintf("The only stop to get your %s tickets", s)
+}
+
+func bookTicket(name string, count uint8, leftTickets *uint8) {
+	if *leftTickets == 0 {
+		fmt.Println("Conference is sold out! No more tickets left.")
+		return
+	}
+
+	*leftTickets = *leftTickets - count
+	fmt.Printf("%s booked %d tickets. %d tickets left\n", name, count, *leftTickets)
 }
