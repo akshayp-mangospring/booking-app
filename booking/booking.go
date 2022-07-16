cf7a03c8268cad5ed0011fadc28243e21ac9ef03
akshayp-mangospring <akshayp@mangospring.com>
2022-07-16 17:57:10 +0530

    Pass in struct instead of separate arguments to `bookTicket` function

--- booking/booking.go
+++ booking/booking.go
@@ -6,22 +6,23 @@ import (
 )
 
 // Usage of pointers for altering data directly at memory location
-func BookTicket(name string, count uint8, email string, c *Conf) {
+func BookTicket(u User, c *Conf) {
 	// Type conversion from `uint8` to `int`
 	if utils.IsZero(int(c.Tickets)) {
 		printNoTicketsLeft()
 		return
 	}
 
-	if count > c.Tickets {
-		fmt.Printf("%s booked the last %d tickets out of %d tickets requested. 0 tickets left.\n", name, c.Tickets, count)
-		c.users = append(c.users, User{Name: name, Email: email, Tickets: c.Tickets})
+	if u.Tickets > c.Tickets {
+		fmt.Printf("%s booked the last %d tickets out of %d tickets requested. 0 tickets left.\n", u.Name, c.Tickets, u.Tickets)
+		u.Tickets = c.Tickets
+		c.users = append(c.users, u)
 		c.Tickets = 0
 		printNoTicketsLeft()
 		return
 	}
 
-	c.Tickets = c.Tickets - count
-	c.users = append(c.users, User{Name: name, Email: email, Tickets: count})
-	fmt.Printf("%s booked %d tickets. %d tickets left\n", name, count, c.Tickets)
+	c.Tickets = c.Tickets - u.Tickets
+	c.users = append(c.users, u)
+	fmt.Printf("%s booked %d tickets. %d tickets left\n", u.Name, u.Tickets, c.Tickets)
 }
--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 			continue
 		}
 
-		booking.BookTicket(user.Name, user.Tickets, user.Email, &conf)
+		booking.BookTicket(user, &conf)
 
 		// Breaks the infinite loop
 		if utils.IsZero(int(conf.Tickets)) {
